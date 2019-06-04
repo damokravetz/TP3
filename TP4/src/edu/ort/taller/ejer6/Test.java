7b46fbcfdddfbc4d958ab8960d46be8c95d075a6
damokravetz <49257250+damokravetz@users.noreply.github.com>
2019-06-04 19:48:18 -0300

    updates

--- TP4/src/edu/ort/taller/ejer6/Test.java
+++ TP4/src/edu/ort/taller/ejer6/Test.java
@@ -1,18 +1,22 @@
 package edu.ort.taller.ejer6;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Tienda miTienda=new Tienda();
 		miTienda.agregarCliente("Juan", "Perez", 29473648, "juanperez@gmail.com", "Av callao 253");
-		Libro l=new Libro("Gabriel Garcia Marquez", "100 aÒos de soledad", "Planeta");
+		Libro l=new Libro("Gabriel Garcia Marquez", "100 a√±os de soledad", "Planeta");
 		miTienda.agregarLibro(l);
-		miTienda.agregarPedido(29473648, "Gabriel Garcia Marquez", "100 aÒos de soledad", "Planeta");
-		System.out.println((Calendar.DATE*100000)+(Calendar.MONTH*10000)+(Calendar.YEAR));
-		
+		miTienda.agrgarStock("Gabriel Garcia Marquez", "100 a√±os de soledad", "Planeta", 10);
+		miTienda.agregarPedido(29473648, "Gabriel Garcia Marquez", "100 a√±os de soledad", "Planeta");
+		miTienda.procesarPedidos();
+		miTienda.informarPedidos();
+		miTienda.informarStock();	
 	}
 
 }
